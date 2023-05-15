c18d5fc9f64163e1c81d0d7b962f541711950d17
Yousuf <yousuf.kuet08@gmail.com>
2023-05-16 01:05:18 +0600

    Add signup screen

---
+++ learnGram/ViewControllers/SignupViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SignupViewController.swift
+//  learnGram
+//
+//  Created by Yousuf on 16/5/23.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+    
+    static let storyboardId = "SignupViewController"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
