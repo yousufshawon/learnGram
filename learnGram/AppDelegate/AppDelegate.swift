a2ce41c2469d753bd7b484d7fc77be0af69f809b
Yousuf <yousuf.kuet08@gmail.com>
2023-05-20 00:49:40 +0600

    Add IQKeyboardManager as dependency for keyboard manage

--- learnGram/AppDelegate/AppDelegate.swift
+++ learnGram/AppDelegate/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import IQKeyboardManager
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         print("AppDelegate: application, didFinishLaunchingWithOptions")
+        
+        IQKeyboardManager.shared().isEnabled = true
+        
         return true
     }
 
